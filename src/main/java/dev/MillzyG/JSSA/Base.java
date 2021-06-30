2b18d92f638d2879d45d3663755d96511e030782
MillzyG <64277238+MillzyG@users.noreply.github.com>
2021-06-30 18:02:13 +0100

    Create Base.java

---
+++ src/main/java/dev/MillzyG/JSSA/Base.java
@@ -0,0 +1,8 @@
+package dev.MillzyG.JSSA;
+
+/**
+ * Contains variables used by many parts of the library
+ */
+public class Base {
+    public static String baseURL = "https://new.scoresaber.com";
+}
