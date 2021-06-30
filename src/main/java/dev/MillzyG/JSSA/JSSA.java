deeb794c5abcad9889847dc3101c6bcf0a9dc512
MillzyG <64277238+MillzyG@users.noreply.github.com>
2021-06-30 18:02:09 +0100

    Update JSSA.java

--- src/main/java/dev/MillzyG/JSSA/JSSA.java
+++ src/main/java/dev/MillzyG/JSSA/JSSA.java
@@ -1,9 +1,11 @@
 package dev.MillzyG.JSSA;
 
-import java.io.IOException;
-
 public class JSSA {
-    public static Player GetPlayer(String id) throws IOException {
-        return new Player(id);
+    public Players Players;
+    public Requests Requests;
+
+    public JSSA() {
+        this.Players = new Players();
+        this.Requests = new Requests();
     }
 }
