54daf9a4bc6456194b26dae6375b919f6659dac6
MillzyG <64277238+MillzyG@users.noreply.github.com>
2021-07-03 09:02:08 +0100

    Loop example

--- src/main/java/Example/Example.java
+++ src/main/java/Example/Example.java
@@ -6,29 +6,31 @@ import java.io.InputStreamReader;
 
 public class Example {
     public static void main(String[] args) throws IOException {
-        System.out.println("What Example would you like to see?");
-        System.out.println("[1] GetPlayer()");
-        System.out.println("[2] SearchPlayers()");
-        System.out.println("[3] GetRequest()");
-        System.out.print("Enter a number: ");
+        while (true) {
+            System.out.println("What Example would you like to see?");
+            System.out.println("[1] GetPlayer()");
+            System.out.println("[2] SearchPlayers()");
+            System.out.println("[3] GetRequest()");
+            System.out.print("Enter a number: ");
 
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in)
-        );
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
 
-        int code = Integer.parseInt(reader.readLine());
+            int code = Integer.parseInt(reader.readLine());
 
-        switch (code) {
-            case 1:
-                GetPlayerExample.GetPlayer();
-                break;
-            case 2:
-                SearchPlayersExample.SearchPlayers();
-                break;
-            case 3:
-                GetRequestExample.GetRequest();
-            default:
-                System.out.println("Invalid Input...");
+            switch (code) {
+                case 1:
+                    GetPlayerExample.GetPlayer();
+                    break;
+                case 2:
+                    SearchPlayersExample.SearchPlayers();
+                    break;
+                case 3:
+                    GetRequestExample.GetRequest();
+                default:
+                    System.out.println("Invalid Input...");
+            }
         }
     }
 }
