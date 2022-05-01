d9170f54059f1de9e3d1de582b7969e5a3f20c20
Millzy <millzyyt08@gmail.com>
2022-05-01 23:15:25 +0100

    rewrite

--- src/main/java/dev/millzyg/jssa/Base.java
+++
@@ -1,9 +0,0 @@
-package dev.millzyg.jssa;
-
-/**
- * Contains base variables used by many parts of the library
- * @since v0.1.0
- */
-public class Base {
-    public static String baseURL = "https://new.scoresaber.com";
-}
